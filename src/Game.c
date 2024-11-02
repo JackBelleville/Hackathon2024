b84c4104d4aa6a1c99a219b60642a14db255a7cb
Jack Belleville <jrbbbc@umsystem.edu>
2024-11-02 18:28:20 -0500

    created print and initialize array

--- src/Game.c
+++ src/Game.c
@@ -2,16 +2,31 @@
 
 #define SIZE 20
 
-int main(void)
+void initializeArray(char arr[][SIZE])
 {
-    char map[SIZE][SIZE];
     for(int row = 0; row < SIZE; row++)
     {
         for(int col = 0; col < SIZE; col++)
         {
-            map[row][col] = 'a';
-            printf("[%c]", map[row][col]);
+            arr[row][col] = 'a';
+        }
+    }
+}
+void printArray(char arr[][SIZE])
+{
+     for(int row = 0; row < SIZE; row++)
+    {
+        for(int col = 0; col < SIZE; col++)
+        {
+            printf("[%c]",arr[row][col]);
         }
         printf("\n");
     }
+}
+
+int main(void)
+{
+    char map[SIZE][SIZE];
+    initializeArray(map);
+    printArray(map);
 }
